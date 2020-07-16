d36387a845a07f8bc7538c157860968ae1fccdb6
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2020-07-16 17:47:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd ~
 mkdir FreeplayGBAcm3
-#git clone https://github.com/julenvitoria/FreeplayGBAcm3
+git clone https://github.com/julenvitoria/FreeplayGBAcm3
 cd FreeplayGBAcm3
 crontab mycron
 cp emulators.cfg /opt/retropie/configs/all
