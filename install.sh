da84660922646ef3459ddb63224eee0e11008bfe
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2020-07-16 19:20:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ chmod +x /home/pi/RetroPie/retropiemenu/Actualizar-Addons/Update.sh
 if grep -q "Actualizar-Addons" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
         echo "Actualizar Addons ya se encuenra en el menu"
 else
-        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Actualizar-Addons/Update.sh</path>\n\t\t<name>Actualizar Addons de la Imag$
+        's|</gameList>|\t<game>\n\t\t<path>./Actualizar-Addons/Update.sh</path>\n\t\t<name>Actualizar Addons de la Imagen</name>\n\t\t<desc>Script para la actualizacion de las diferentes opciones y configuraciones de la imagen FreeplayGBAcm3</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
 
