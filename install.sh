ef8a2476c9097922eea3ba7d159c932e7fe79da5
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2020-07-16 18:02:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 cd ~
 mkdir FreeplayGBAcm3
 git clone https://github.com/julenvitoria/FreeplayGBAcm3
@@ -7,11 +8,11 @@ crontab mycron
 cp emulators.cfg /opt/retropie/configs/all
 
 
-mkdir -p "/home/pi/RetroPie/retropiemenu/Freeplay Options"
-cp /home/pi/Freeplay/Freeplay-Support/OptimalLCDSettings.sh "/home/pi/RetroPie/retropiemenu/Freeplay Options/OptimalLCDSettings.sh"
+mkdir -p "/home/pi/RetroPie/retropiemenu/Actualizar-Addons"
+cp /home/pi/FreeplayGBAcm3/Update.sh "/home/pi/RetroPie/retropiemenu/Actualizar-Addons/Update.sh"
 
-if grep -q "Freeplay Optimal LCD Settings" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
-	echo "Optimal LCD Settings Changer already in menu"
+if grep -q "Actualizar-Addons" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
+	echo "Actualizar Addons ya se encuenra en el menu"
 else
-	sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Freeplay Options/OptimalLCDSettings.sh</path>\n\t\t<name>Freeplay Optimal LCD Settings</name>\n\t\t<desc>Switch between optimal LCD settings and HDMI compatibility</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+	sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Actualizar-Addons/Update.sh</path>\n\t\t<name>Actualizar Addons de la Imagen</name>\n\t\t<desc>Script para la actualizacion de las diferentes opciones y configuraciones de la imagen FreeplayGBAcm3</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
