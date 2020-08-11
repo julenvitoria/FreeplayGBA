6132e40c7a16eda54968ca733cc0b03fb07b6171
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2020-08-11 19:19:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,8 @@ if [ -d /home/pi/RetroPie/retropiemenu/Update-Addons ]; then
 else
         mkdir /home/pi/RetroPie/retropiemenu/Update-Addons
 fi
-#if grep -q "Update Addons" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
-#        echo "Directory update addons was created yet"
-#else
-#        mkdir /home/pi/RetroPie/retropiemenu/Update-Addons
-#fi
-#mkdir /home/pi/RetroPie/retropiemenu/Update-Addons
-cp /home/pi/FreeplayGBAcm3/Update.sh /home/pi/RetroPie/retropiemenu/Update-Addons
+wget -O- https://raw.githubusercontent.com/julenvitoria/FreeplayGBAcm3Addons/master/UpdateAddons.sh>/home/pi/RetroPie/retropiemenu/Update-Addons/UpdateAddons.sh
+#cp /home/pi/FreeplayGBAcm3/Update.sh /home/pi/RetroPie/retropiemenu/Update-Addons
 #cp /home/pi/FreeplayGBAcm3/ActualizarSistema.sh /home/pi/RetroPie/retropiemenu/Update-Addons
 chmod +x /home/pi/RetroPie/retropiemenu/Update-Addons/Update.sh
 wget -O- https://raw.githubusercontent.com/julenvitoria/FreeplayGBA-UpdateUpgradeSystem/master/UpdateUpgradeSystem.sh>/home/pi/RetroPie/retropiemenu/Update-Addons/UpdateUpgradeSystem.sh
@@ -21,7 +16,7 @@ chmod +x /home/pi/RetroPie/retropiemenu/Update-Addons/UpdateUpgradeSystem.sh
 if grep -q "Update Addons" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
         echo "Update Addons is in the gamelist.xml yet"
 else
-        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Update-Addons/Update.sh</path>\n\t\t<name>Update Addons</name>\n\t\t<desc>Script for la updating options and configs of the image FreeplayGBAcm3</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+        sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Update-Addons/UpdateAddons.sh</path>\n\t\t<name>Update Addons</name>\n\t\t<desc>Script for la updating options and configs of the image FreeplayGBAcm3</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
 if grep -q "Update and Upgrade System" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
         echo "Update and Upgrade System is in the gamelist.xml yet"
