669ae21ae9e92386348ffbcbdec2ac764a875715
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2020-08-12 18:46:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if [ -d /home/pi/RetroPie/retropiemenu/Update-Addons ]; then
 else
         mkdir /home/pi/RetroPie/retropiemenu/Update-Addons
 fi
-#Update Addons
+#Install Update Addons
 wget -O- https://raw.githubusercontent.com/julenvitoria/FreeplayGBAcm3Addons/master/UpdateAddons.sh>/home/pi/RetroPie/retropiemenu/Update-Addons/UpdateAddons.sh
 chmod +x /home/pi/RetroPie/retropiemenu/Update-Addons/UpdateAddons.sh
 if grep -q "UpdateAddons.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
@@ -16,7 +16,7 @@ if grep -q "UpdateAddons.sh" /opt/retropie/configs/all/emulationstation/gamelist
 else
         sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Update-Addons/UpdateAddons.sh</path>\n\t\t<name>Update Addons</name>\n\t\t<desc>Script for la updating options and configs of the image FreeplayGBAcm3</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
-#Update and Upgrade System
+#Install Update and Upgrade System
 wget -O- https://raw.githubusercontent.com/julenvitoria/FreeplayGBA-UpdateUpgradeSystem/master/UpdateUpgradeSystem.sh>/home/pi/RetroPie/retropiemenu/Update-Addons/UpdateUpgradeSystem.sh
 chmod +x /home/pi/RetroPie/retropiemenu/Update-Addons/UpdateUpgradeSystem.sh
 if grep -q "UpdateUpgradeSystem.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
@@ -24,7 +24,7 @@ if grep -q "UpdateUpgradeSystem.sh" /opt/retropie/configs/all/emulationstation/g
 else
         sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Update-Addons/UpdateUpgradeSystem.sh</path>\n\t\t<name>Update and Upgrade System</name>\n\t\t<desc>Script for Update and Upgrade System automatically</desc>\n\t\t<image></image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
-#Update Emulators.cfg
+#Install Update Emulators.cfg
 wget -O- https://raw.githubusercontent.com/julenvitoria/FreeplayGBAcm3-Emulators.cfg/master/UpdateEmulators.sh>/home/pi/RetroPie/retropiemenu/Update-Addons/UpdateEmulators.sh
 if grep -q "UpdateEmulators.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
         echo "Update Emulators is in the gamelist.xml yet"
