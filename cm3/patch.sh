eccee6067aa663f048aa65beb2f61d898bc2dec2
Julen Sanchez Rosales <julenvitoria@me.com>
2020-08-17 17:42:02 +0200

    patch.sh Update

--- cm3/patch.sh
+++ cm3/patch.sh
@@ -6,6 +6,6 @@ sudo apt update
 sudo apt upgrade -y
 #Install packages that have been kept back. If packages aren't kept back nothing happens...
 sudo apt install raspberrypi-kernel raspberrypi-kernel-headers libraspberrypi-bin libraspberrypi-dev libraspberrypi-doc libraspberrypi0 raspberrypi-bootloader -y --allow-change-held-packages
-cd Freeplay
+cd ~/Freeplay
 cd mk_arcade_joystick_rpi
 ./install.sh
