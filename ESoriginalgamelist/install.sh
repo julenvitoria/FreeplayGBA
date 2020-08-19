4f546dd06db8b7d5142f07675beb790c0408b02d
Julen Sanchez Rosales <julenvitoria@me.com>
2020-08-19 19:38:41 +0200

    install.sh

--- ESoriginalgamelist/install.sh
+++ ESoriginalgamelist/install.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-cd /home/pi
-if [ -d /home/pi/tmp ]; then
-        sudo rm -R /home/pi/tmp
-        mkdir /home/pi/tmp
-else
-        mkdir /home/pi/tmp
-fi
-cd tmp
-#Downloading and copying emulators configs
 echo "Downloading and copying original retropie section gamelist.xml and reinstalling Addons Menu..."
 sleep 2
 wget -O- https://raw.githubusercontent.com/julenvitoria/FreeplayGBA/master/ESoriginalgamelist/gamelist.xml>/opt/retropie/configs/all/emulationstation/gamelists/retropie/
