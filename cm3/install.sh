bc8f6d5bad97502d7f27cd988e4589e507f7e86f
Julen Sanchez Rosales <julenvitoria@me.com>
2020-08-25 07:14:26 +0200

    commit

--- cm3/install.sh
+++ cm3/install.sh
@@ -4,10 +4,6 @@ clear
 cd /home/pi
 sudo apt update
 sudo apt upgrade -y
-#Install packages that have been kept back. If not nothing happens...
-#sudo apt install raspberrypi-kernel -y --allow-change-held-packages
-#sudo apt install raspberrypi-kernel-headers -y --allow-change-held-packages
-#sudo apt install libraspberrypi-bin libraspberrypi-dev libraspberrypi-doc libraspberrypi0 raspberrypi-bootloader -y --allow-change-held-packages
 #Install subversion (SVN) for the normal functionality of github-downloader script
 sudo apt install -y subversion
 #Install autoconf.cfg for sound on Mupen64Plus
