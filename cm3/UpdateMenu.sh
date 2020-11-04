12abae0272aadc8a0e4f8b5aca2fa6d359a9e429
Julen Sanchez Rosales <julenvitoria@hotmail.es>
2020-11-05 00:08:21 +0100

    commit

--- cm3/UpdateMenu.sh
+++ cm3/UpdateMenu.sh
@@ -49,7 +49,7 @@ else
 fi
 
 #Install Super Retroboy theme and launchings
-wget -O- https://raw.githubusercontent.com/julenvitoria/FreeplayGBA-SuperRetroBoyTheme/master/STBtheme.sh>/home/pi/RetroPie/retropiemenu/Update-Addons/ThemePlusLaunchings.sh
+wget -O- https://raw.githubusercontent.com/julenvitoria/FreeplayGBA-SuperRetroBoyTheme/master/SRBtheme.sh>/home/pi/RetroPie/retropiemenu/Update-Addons/ThemePlusLaunchings.sh
 chmod +x /home/pi/RetroPie/retropiemenu/Update-Addons/SRBtheme.sh
 if grep -q "SRBtheme.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
         echo "SRBtheme is in the gamelist.xml yet"
